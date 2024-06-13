d913be8cb0fcc45221de3129df1b85a18f87433a
Scardell <153379926+scardell@users.noreply.github.com>
2024-06-13 17:34:58 +0200

    mas scripts

---
+++ django-0-starting/ex03/capital_city.py
@@ -0,0 +1,33 @@
+
+import  sys
+
+
+def print_capital_city(key: str):
+    states = {
+        "Oregon": "OR",
+        "Alabama": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+    key = states.get(key)
+    if not key:
+        print("Unknown state")
+    else:
+        return print(capital_cities.get(key))
+
+
+
+
+def main():
+    if len(sys.argv) == 2:
+        print_capital_city(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
